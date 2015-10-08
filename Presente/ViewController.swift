87d781bb50e5e4579b91096799d8c26fd312d419
Evan <ewinelan@andrew.cmu.edu>
2015-10-08 00:35:50 -0400

    Initial commit

---
+++ Presente/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Presente
+//
+//  Created by Evan Wineland on 10/8/15.
+//  Copyright © 2015 Evan Wineland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
