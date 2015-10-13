7770e180323383f9127d23cd73b4ad6884f76f21
Evan <ewinelan@andrew.cmu.edu>
2015-10-13 18:54:28 -0400

    Setting up activity view indicator

--- Presente/CustomLogInViewController.swift
+++ Presente/CustomLogInViewController.swift
@@ -14,10 +14,16 @@ class CustomLogInViewController: UIViewController {
     @IBOutlet var usernameField: UITextField!
     @IBOutlet var passwordField: UITextField!
     
+    var actInd : UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.actInd.center = self.view.center
+        self.actInd.hidesWhenStopped = true
+        self.actInd.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        
+        view.addSubview(self.actInd)
     }
 
     override func didReceiveMemoryWarning() {
