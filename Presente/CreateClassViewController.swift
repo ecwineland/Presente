99a2464f7e2caae7a5b9993c0e21cf721dc05b50
Evan <ewinelan@andrew.cmu.edu>
2015-10-19 10:33:54 -0400

    Working on create new class method

--- Presente/CreateClassViewController.swift
+++ Presente/CreateClassViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class CreateClassViewController: UIViewController {
+    
+    @IBOutlet var clssName: UITextField!
+    @IBOutlet var clssNum: UITextField!
+    @IBOutlet var clssDesc: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +26,32 @@ class CreateClassViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    @IBAction func cancel(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func createClass(sender: AnyObject) {
+        
+        // Create error message if one or more fields is empty
+        if (clssName.text!.isEmpty || clssNum.text!.isEmpty || clssDesc.text!.isEmpty ) {
+            
+            let alert = UIAlertView()
+            alert.title = "Missing fields"
+            alert.message = "Fill in all fields to create the new class"
+            alert.addButtonWithTitle("Ok")
+            alert.show()
+            
+        // Create new class
+        } else {
+            var newClass = PFObject(className: "Class")
+            newClass.setObject(clssName.text!, forKey: "Name")
+            newClass.setObject(clssNum.text!, forKey: "Num")
+            newClass.setObject(<#T##object: AnyObject##AnyObject#>, forKey: <#T##String#>)
+            
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
