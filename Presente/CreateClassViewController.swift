3f8e87560f8c07e19c0b2b00a2ca369c467a50e1
Evan <ewinelan@andrew.cmu.edu>
2015-10-21 08:32:53 -0400

    Added start and stop functions in appDelegate.  Going to make sure that behavior hasn't changed

--- Presente/CreateClassViewController.swift
+++ Presente/CreateClassViewController.swift
@@ -14,6 +14,7 @@ class CreateClassViewController: UIViewController, ESTBeaconManagerDelegate {
     @IBOutlet var clssNum: UITextField!
     @IBOutlet var clssDesc: UITextField!
     
+    let beaconManager = ESTBeaconManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
